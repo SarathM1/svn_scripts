b4636291ee19a0b1be3fe9878c3e23a4b1a2d871
Home PC <sarathm00@gmail.com>
2018-03-24 18:17:46 +0530

    Created class

--- proc.py
+++ proc.py
@@ -5,29 +5,30 @@ import pandas as pd
 import re
 import yodl
 
-
-
-def run_cmd(each_dir):
-    cmd = "svn info {}".format(each_dir)
+def run_cmd(cmd):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     return output, err
 
-def parse(output, pattern):
-    match = re.search('(?<=' + pattern + ': ).*$', output, re.MULTILINE)
-    if match:
-        res = match.group()
-        return res
-    else:
-        print 'No match'
+class svn_info():
+    def __init__(self, each_dir):
+        self.data_row = {}
+        cmd = "svn info {}".format(each_dir)
+        output, err = run_cmd(cmd)
+        self.data_row['revision_num'] =self.parse(output, 'Revision')
+        self.data_row['date'] =self.parse(output, 'Last Changed Date')
+        self.data_row['author'] =self.parse(output, 'Last Changed Author')
 
-def svn_info(each_dir):
-    data_row = {}
-    output, err = run_cmd(each_dir)
-    data_row['revision_num'] =parse(output, 'Revision')
-    data_row['date'] =parse(output, 'Last Changed Date')
-    data_row['author'] =parse(output, 'Last Changed Author')
-    return data_row
+    def parse(self, output, pattern):
+        match = re.search('(?<=' + pattern + ': ).*$', output, re.MULTILINE)
+        if match:
+            res = match.group()
+            return res
+        else:
+            print 'No match'
+
+def svn_unstaged(each_dir):
+    cmd = "svn status ~/project1_work | awk '/^?/ {print $2}' | wc -l"
 
 def main():
     with open('./test.yml', 'r') as stream:
@@ -37,11 +38,13 @@ def main():
             data = []
             # for each_dir in dirs:
             for key, module in dirs['modules'].iteritems():
-                row = svn_info(module['path'])
+                info = svn_info(module['path'])
+                row = info.data_row
                 row['path'] = module['path']
                 row['owner'] = module['owner']
                 data.append(row)
             df = pd.DataFrame(data)
+            df.to_csv("repo_status.csv")
             print "Data: \n", df
         except yaml.YAMLError as exc:
             print(exc)
