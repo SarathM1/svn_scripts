8568517971115544449accf50ed0c825bc42ace3
Home PC <sarathm00@gmail.com>
2018-03-24 17:59:53 +0530

    Deleted test code

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import yaml
-import yodl
-
-with open('./test.yml', 'r') as stream:
-    # data = yaml.load(stream)
-    data = yaml.load(stream, yodl.OrderedDictYAMLLoader)
-    for key, module in data['modules'].iteritems():
-        print key, module['path'], module['owner']
