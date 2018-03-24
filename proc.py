ef7d978b2cad5df4a9a0ba9980e989465d4bb28e
Home PC <sarathm00@gmail.com>
2018-03-24 18:32:53 +0530

    Cleaned up the code

--- proc.py
+++ proc.py
@@ -12,12 +12,12 @@ def run_cmd(cmd):
 
 class svn_info():
     def __init__(self, each_dir):
-        self.data_row = {}
+        self.count = {}
         cmd = "svn info {}".format(each_dir)
         output, err = run_cmd(cmd)
-        self.data_row['revision_num'] =self.parse(output, 'Revision')
-        self.data_row['date'] =self.parse(output, 'Last Changed Date')
-        self.data_row['author'] =self.parse(output, 'Last Changed Author')
+        self.count['revision_num'] =self.parse(output, 'Revision')
+        self.count['date'] =self.parse(output, 'Last Changed Date')
+        self.count['author'] =self.parse(output, 'Last Changed Author')
 
     def parse(self, output, pattern):
         match = re.search('(?<=' + pattern + ': ).*$', output, re.MULTILINE)
@@ -27,8 +27,23 @@ class svn_info():
         else:
             print 'No match'
 
-def svn_unstaged(each_dir):
-    cmd = "svn status ~/project1_work | awk '/^?/ {print $2}' | wc -l"
+class svn_st():
+    def __init__(self, each_dir):
+        self.data_row = {}
+        cmd = "svn status {}".format(each_dir)
+        output, err = run_cmd(cmd)
+
+        self.count = {}
+        self.count['unversioned'] =self.parse(output)
+        self.unversioned_cnt = self.parse(output, 'Revision')
+
+    def parse(self, output):
+        match = re.search('^\? .*$', output, re.MULTILINE)
+        if match:
+            res = match.group()
+            print res
+        else:
+            print 'No match'
 
 def main():
     with open('./test.yml', 'r') as stream:
@@ -38,10 +53,14 @@ def main():
             data = []
             # for each_dir in dirs:
             for key, module in dirs['modules'].iteritems():
-                info = svn_info(module['path'])
-                row = info.data_row
+                row = {}
                 row['path'] = module['path']
                 row['owner'] = module['owner']
+
+                info = svn_info(module['path'])
+                for each_key, each_val in info.count.iteritems():
+                    row[each_key] = each_val
+
                 data.append(row)
             df = pd.DataFrame(data)
             df.to_csv("repo_status.csv")
